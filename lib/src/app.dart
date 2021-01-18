aa65e7791db6967ebb7fac94c1796b4bff90119d
luisalbertmschz <luisalbertmsanchez@gmail.com>
2021-01-18 01:42:35 -0400

    Rewrite de Structura App y Org de ficheros 🚀

---
+++ lib/src/app.dart
@@ -0,0 +1,31 @@
+
+
+import 'package:flutter/material.dart';
+
+
+class MyApp extends  StatelessWidget {
+
+@override
+
+build( context ) {
+
+
+  return MaterialApp(
+
+
+  home: 
+  
+  Center(
+
+  child:Text('Hola Mundo') ,
+
+  )
+
+
+  );
+
+
+
+}
+
+}
